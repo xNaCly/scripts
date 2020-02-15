db785cc958a4d1344a3931dec34f18892b8868b5
xNaCly <47723417+xNaCly@users.noreply.github.com>
2020-02-15 16:21:57 +0000

    Update tokenchecker.py

--- discord/tokenchecker/tokenchecker.py
+++ discord/tokenchecker/tokenchecker.py
@@ -2,7 +2,7 @@ import requests
 import json
 def main_main():
 	auth = input("Token: ")
-	write_db(auth)
+	#write_db(auth)
 	payload = {'Authorization':f'{auth}'}
 	r = requests.get('https://discordapp.com/api/v6/users/@me', headers=payload)
 	x = json.loads(r.content)
@@ -10,10 +10,10 @@ def main_main():
 	input('\npress any button to continue... ')
 	main_main_main()
 
-def write_db(token):
-	f = open("auth","a")
-	f.write("{0}\n".format(token))
-	f.close()
+#def write_db(token):
+	#f = open("auth","a")
+	#f.write("{0}\n".format(token))
+	#f.close()
 
 def main_main_main():
 	main_main()
