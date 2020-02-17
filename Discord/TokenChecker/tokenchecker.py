27a90c5704e90a071c83e427c7e4849eac834d9f
xNaCly <47723417+xNaCly@users.noreply.github.com>
2020-02-17 15:36:03 +0000

    Update and rename discord/tokenchecker/tokenchecker.py to Discord/TokenChecker/tokenchecker.py

--- Discord/TokenChecker/tokenchecker.py
+++ Discord/TokenChecker/tokenchecker.py
@@ -2,19 +2,12 @@ import requests
 import json
 def main_main():
 	auth = input("Token: ")
-	#write_db(auth)
 	payload = {'Authorization':f'{auth}'}
 	r = requests.get('https://discordapp.com/api/v6/users/@me', headers=payload)
 	x = json.loads(r.content)
 	print(json.dumps(x, indent=4))
 	input('\npress any button to continue... ')
 	main_main_main()
-
-#def write_db(token):
-	#f = open("auth","a")
-	#f.write("{0}\n".format(token))
-	#f.close()
-
 def main_main_main():
 	main_main()
 
