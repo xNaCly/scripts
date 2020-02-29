5ddd597e346fae18e8a49355a34f7849896f3277
xnacly <matteogropp@gmail.com>
2020-02-29 19:23:25 +0100

    changed formating

--- dsb/dsb.py
+++ dsb/dsb.py
@@ -1,16 +1,22 @@
 import pydsb
 import requests
 import json
+
+
 def auth(url):
     r = requests.get(url)
     a = json.loads(r.content)
     global b, c
     b, c = a["ab"], a["cd"]
+
+
 def get_plans(usrnme, pw):
     dsb = pydsb.PyDSB(usrnme, pw)
     d = dsb.get_plans()
     for e in d:
         print(e["uploaded_date"])
         print(e["url"] + "\n")
+
+
 auth("https://raw.githubusercontent.com/xnacly1/auth/master/noauth")
 get_plans(b, c)
--- dsb/dsb_gui.py
+++ dsb/dsb_gui.py
@@ -2,11 +2,15 @@ import pydsb
 import requests
 import json
 from tkinter import *
+
+
 def auth(url):
     r = requests.get(url)
     a = json.loads(r.content)
     global b, c
     b, c = a["ab"], a["cd"]
+
+
 def get_plans(usrnme, pw):
     dsb = pydsb.PyDSB(usrnme, pw)
     d = dsb.get_plans()
@@ -15,9 +19,15 @@ def get_plans(usrnme, pw):
         v.set(e["url"])
         Entry(r, textvariable=v, width=250).pack()
         r.geometry("1000x100")
+
+
 auth("https://raw.githubusercontent.com/xnacly1/auth/master/noauth")
+
+
 def packed():
-	get_plans(b,c)
+    get_plans(b, c)
+
+
 r = Tk()
 r.title("Tkinter Test")
 r.geometry("200x200")
