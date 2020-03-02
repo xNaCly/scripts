467b9e1ec39ab3cf307bd5708f6d5fd9ec38265b
xnacly <matteogropp@gmail.com>
2020-03-02 16:22:52 +0100

    added uuid_to_name & name_to_uuid

--- Hypixel/hypixel_api.py
+++ Hypixel/hypixel_api.py
@@ -14,12 +14,15 @@ from config import base_url as url
 from colorama import Fore, Back, Style
 colorama.init()
 
+
 def rerun():
     s("cls")
     start()
 
+
 def name_to_uudi(username):
-    burl = "https://api.mojang.com/users/profiles/minecraft/{}".format(username)
+    burl = "https://api.mojang.com/users/profiles/minecraft/{}".format(
+        username)
     r = requests.get(burl)
     if r.status_code == 200:
         r_json = json.loads(r.content)
@@ -27,7 +30,7 @@ def name_to_uudi(username):
         s("cls")
         print("Username " + Fore.GREEN +
               f"{username}" + Style.RESET_ALL + " found connected with")
-        print(Fore.GREEN + f"{uuid}")
+        print(Fore.GREEN + f"{uuid}" + Style.RESET_ALL)
         return uuid
     else:
         print(Fore.RED + "There was a Problem, either wrong 'Username' or no internet connection" +
@@ -35,17 +38,30 @@ def name_to_uudi(username):
         input("continue: ")
         rerun()
 
+
+def uuid_to_name(uuid):
+    burl = "https://sessionserver.mojang.com/session/minecraft/profile/{}".format(
+        uuid)
+    r = requests.get(burl)
+    a = json.loads(r.content)
+    name = a["name"]
+    return name
+
+
 def hypixel_main_request(uuid, key, url):
     url = url + f"skyblock/profile?key={key}&profile={uuid}"
     r = requests.get(url)
     a = json.loads(r.text)
+
 
 def start():
     name = input("Username: ")
     if name:
         uuid = name_to_uudi(name)
         hypixel_main_request(uuid, key, url)
+        uuid_to_name(uuid)
     else:
         return rerun()
-    
+
+
 start()
