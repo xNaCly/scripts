2fe2e4c0b1d28443a98dc4c9bf27872471e0ec2a
xnacly <matteogropp@gmail.com>
2020-05-02 20:08:29 +0200

    cleaned up rpc.py & added final draft covid.py

--- Covid19/covid.py
+++ Covid19/covid.py
@@ -1,6 +1,8 @@
 import requests
 import json
-response_object = """{
+import os
+
+"""{
 	"Global":{
 		"NewConfirmed":83608,
 		"TotalConfirmed":2400051,
@@ -11,9 +13,60 @@ response_object = """{
 		},
 	"Date":"2020-04-20T17:48:21Z"
 	}"""
-r = requests.get("https://api.covid19api.com/summary")
-rr = json.loads(str(r.text))
-print("\n")
-for x in rr['Global']:
-	print(f"{x}: {rr['Global'][x]}")
-print(rr['Date'])
+
+def requestStats():
+	try:
+		r = requests.get("https://api.covid19api.com/summary")
+		rr = json.loads(str(r.text))
+		return rr
+	except:
+		print("! While trying to establish a internet-connection, \nan error occured, \ntry disabling your firewall, or adding this program to the whitelist !")
+		exit()
+
+RESPONSE = requestStats()
+
+def globalStats():
+	rr = RESPONSE
+	for x in rr['Global']:
+		print(f"{x}: {rr['Global'][x]}")
+	print(rr['Date'])
+
+def displayAllCoutries():
+	rr = RESPONSE
+	for x in rr['Countries']:
+		print(f"{x['Country']} | {x['CountryCode']}")
+
+def displayOneCountry(Country):
+	rr = RESPONSE
+	
+	for x in rr['Countries']:
+		if x['Country'].lower() == Country.lower(): #== Country or x['Slug'] == Country or x['CountryCode'] == Country:
+			for y in x:
+				print(f"{y}: {x[y]}")
+			break
+
+def main():
+	print("""
++- COVID19 - info - script -+
+			+-xnacly-+	
+arguments: global, allCountries, oneCountry
+	\n\n
+	""")
+	cmd = input()
+	if cmd == "global":
+		globalStats()
+		fallbackFun()
+	elif cmd == "allCountries":
+		displayAllCoutries()
+		fallbackFun()	
+	elif cmd == "oneCountry":
+		coun = input("Country: ")
+		displayOneCountry(coun)
+		fallbackFun()
+
+def fallbackFun():
+	input("\nPress any Button to get back... ")
+	os.system("cls")
+	main()
+
+main()
--- Discord/RichPresence/rpc.py
+++ Discord/RichPresence/rpc.py
@@ -1,7 +1,6 @@
 # xnacly
 from pypresence import Presence
 from win32gui import GetWindowText, GetForegroundWindow
-from time import sleep as s
 import time
 
 
@@ -15,26 +14,21 @@ def log(process):
 timer = int(time.time())
 RPC = Presence("651125823457329152")
 RPC.connect()
-prc_name = ""  # input()
-s(2)
-if prc_name == "":
-    while True:
-        penis = GetWindowText(GetForegroundWindow())
-        if penis == "":
-            penis = "error 404"
+time.sleep(2)
 
-        RPC.update(state=f"{penis}", start=timer, large_image="uwu",
-                   large_text=f"{penis}", small_image="uwu", small_text="made by xNaCly#6370")
-        print(penis + " | " + str(timer))
-        log(penis)
-        s(15)
-else:
-    while True:
-        penis = prc_name
-        if len(penis) >= 125:
-            penis = "error - task string too long"
-        RPC.update(state=f"{penis}", start=timer, large_image="uwu",
-                   large_text=f"{penis}", small_image="uwu", small_text="made by xNaCly#6370")
-        log(penis)
-        print(penis + " | " + str(timer))
-        s(15)
+while True:
+    windowstring = GetWindowText(GetForegroundWindow())
+
+    if len(windowstring) >= 125:
+        windowstring = "error - task string too long"
+
+    if windowstring == "":
+        windowstring = "error 404"
+
+    RPC.update(state=f"{windowstring}", start=timer, large_image="uwu",
+                   large_text=f"{windowstring}", small_image="uwu", small_text="made by xnacly#6370")
+
+    print(windowstring + " | " + str(timer))
+
+    log(windowstring)
+    time.sleep(15)
