65da657a442497af828c3007a7cb6a31d6e341a9
xnacly <matteogropp@gmail.com>
2020-05-06 18:33:03 +0200

    split code up into front & backend, fixed typo

--- Covid19/covid_backend.py
+++ Covid19/covid_backend.py
@@ -1,20 +1,9 @@
 import requests
 import json
-import os
-from sys import platform
-
-
-clearstring = ""
-if platform == "win32":
-	clearstring = "cls"
-else:
-	clearstring = "clear"
-
 
 def errorLogger(error):
 	with open("log", "a") as f:
 		f.write(f"\n\n{error}")
-
 
 def requestStats():
 	try:
@@ -29,59 +18,67 @@ def requestStats():
 RESPONSE = requestStats()
 
 def globalStats():
-	os.system(clearstring)
-	rr = RESPONSE
-	for x in rr['Global']:
-		print(f"{x}: {rr['Global'][x]}")
-	print(rr['Date'])
+	
+	# NewConfirmed: 90841
+	# TotalConfirmed: 3658565
+	# NewDeaths: 4734
+	# TotalDeaths: 257093
+	# NewRecovered: 84832
+	# TotalRecovered: 1195874
+	# 2020-05-06T16:21:13Z
 
-def displayAllCoutries():
-	os.system(clearstring)
 	rr = RESPONSE
+	c = ""
+	for x in rr['Global']:
+		c += f"{x}: {rr['Global'][x]}\n"
+	c += f"{rr['Date']}"
+	return c
+
+
+def displayAllCountries():
+	
+	# ALA Aland Islands | AX
+	# Afghanistan | AF
+	# Albania | AL
+	# Algeria | DZ
+	# American Samoa | AS
+	# Andorra | AD
+
+	rr = RESPONSE
+	c = ""
 	for x in rr['Countries']:
-		print(f"{x['Country']} | {x['CountryCode']}")
+		c += f"{x['Country']} | {x['CountryCode']}\n"
+	return c
+
 
 def displayOneCountry(Country):
-	os.system(clearstring)
+
+	# Country: United States of America
+	# CountryCode: US
+	# Slug: united-states
+	# NewConfirmed: 0
+	# TotalConfirmed: 1204351
+	# NewDeaths: 0
+	# TotalDeaths: 71064
+	# NewRecovered: 0
+	# TotalRecovered: 189791
+	# Date: 2020-05-06T16:21:13Z
+
 	rr = RESPONSE
+	c = ""
+
 	for x in rr['Countries']:
 		if x['Country'].lower() == Country.lower(): 
 			for y in x:
-				print(f"{y}: {x[y]}")
+				c += f"{y}: {x[y]}\n"
 			break
 		if x['Slug'].lower() == Country.lower():
 			for y in x:
-				print(f"{y}: {x[y]}")
+				c += f"{y}: {x[y]}\n"
 			break
 		if x['CountryCode'].lower() == Country.lower():
 			for y in x:
-				print(f"{y}: {x[y]}")
+				c += f"{y}: {x[y]}\n"
 			break
-def main():
-	print("""
-+- COVID19 - info - script -+
-+-xnacly-+	
-arguments: 
-1: global
-2: allCountries
-3: oneCountry
-	\n\n
-	""")
-	cmd = input()
-	if cmd == "1":
-		globalStats()
-		fallbackFun()
-	elif cmd == "2":
-		displayAllCoutries()
-		fallbackFun()	
-	elif cmd == "3":
-		coun = input("Country: ")
-		displayOneCountry(coun)
-		fallbackFun()
 
-def fallbackFun():
-	input("\nPress any Button to get back... ")
-	os.system(clearstring)
-	main()
-
-main()
+	return c
---
+++ Covid19/covid_cmd.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import os
+from sys import platform
+from covid_backend import displayAllCountries, globalStats, displayOneCountry
+
+
+clearstring = ""
+if platform == "win32":
+	clearstring = "cls"
+else:
+	clearstring = "clear"
+
+
+def main():
+	print("""
++- COVID19 - info - script -+
++-xnacly-+	
+arguments: 
+1: global
+2: allCountries
+3: oneCountry
+	\n\n
+	""")
+	cmd = input()
+	if cmd == "1":
+		print(globalStats())
+		fallbackFun()
+	elif cmd == "2":
+		print(displayAllCountries())
+		fallbackFun()	
+	elif cmd == "3":
+		coun = input("Country: ")
+		print(displayOneCountry(coun))
+		fallbackFun()
+
+def fallbackFun():
+	input("\nPress any Button to get back... ")
+	os.system(clearstring)
+	main()
+
+main()
