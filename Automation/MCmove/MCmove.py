3497678548a019274656d321acd31e69803a303a
xnacly <matteogropp@gmail.com>
2020-06-20 22:27:35 +0200

    added resourcepacks to MCmove

---
+++ Automation/MCmove/MCmove.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+# get current User as name [string]
+user = os.getlogin()
+
+ # path to minecraft mod folder
+path = f"C:\\Users\\{user}\\AppData\\Roaming\\.minecraft"
+
+# get current working dir
+oldpath = os.getcwd() 
+
+print(f"start moving files from:\n{oldpath}\nto\n{path}\n\n")
+# iterate trough all files in oldpath
+for file in os.listdir(): 
+	# split files into array --> test.jar => ["test", "jar"]
+	y = file.split(".")
+
+	# check if file ending is "jar", using len()-1 if other "." are in the file name --> betterframes (1.8.9).jar => ["betterframes ", "(1", "8", "9)", "jar"]
+	if y[len(y)-1] == "jar":
+		# move file from oldpath to modpath
+		shutil.move(f"{oldpath}\\{file}", f"{path}\\mods\\{file}")
+		print(f"\nmoved {file}")
+	
+	if y[len(y)-1] == "zip":
+		# move file from oldpath to txpath
+		shutil.move(f"{oldpath}\\{file}", f"{path}\\resourcepacks\\{file}")
+		print(f"\nmoved {file}")
+
+print("finished moving")
