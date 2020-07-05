032a10a817c0c4092846b15f5ab2021ecd86749d
xnacly <matteogropp@gmail.com>
2020-07-05 21:10:51 +0200

    now supports running from anywhere

--- Automation/Share/test.py
+++ Automation/Share/test.py
@@ -1,10 +1,14 @@
 import os
 import json
 
-with open("extensions.csv", "a") as f:
-	for file in os.listdir():
+user = os.getlogin()
+chromepath = f"C:\\Users\\{user}\\AppData\\Local\\Chromium\\Application"
+
+with open(".\\extensions.csv", "a") as f:
+	filepath = os.listdir(chromepath)[0]
+	for file in os.listdir(f"{chromepath}{filepath}\\Extensions"):
 		try:
-			mainfest = json.loads(open(f"{file}\\manifest.json").read())
+			mainfest = json.loads(open(f"{chromepath}{filepath}\\Extensions\\{file}\\manifest.json").read())
 		except:
 			continue
 		f.write(f"{file}," + mainfest["name"] + "," + mainfest["description"] + "," + mainfest["version"] + "\n") 
