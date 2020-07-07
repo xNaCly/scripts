2bca4d44764150e1701f970c9de625b52b09c537
xnacly <matteogropp@gmail.com>
2020-07-07 09:55:48 +0200

    added file support + cleaned up

--- Text Formatting/Compare/compare.py
+++ Text Formatting/Compare/compare.py
@@ -1,28 +1,59 @@
 # compare strs:
-print("Compare 2 strings and point out differences: ")
-str1 = input("First string: ")
-str2 = input("Second string: ")
-def compare():
-	if str1 == str2:
-		raise ValueError("Strings are identical.")
+class Compare:
+	def __init__(self):
+		self.str1 = ""
+		self.str2 = ""
 
-	result1 = ''
-	result2 = ''
+	def strr(self):
+		self.str1 = input("First string: ")
+		self.str2 = input("Second string: ")
 
-	maxlen=len(str2) if len(str1)<len(str2) else len(str1)
+	def file(self):
+		self.str1 = input("Filename 1: ")
+		self.str2 = input("Filename 2: ")
+		try:
+			with open(f"{self.str1}","r") as f:
+				self.str1 = f.read()
+			with open(f"{self.str2}","r") as f:
+				self.str2 = f.read()
+		except:
+			raise FileNotFoundError(f"files: '{self.str1}' or '{self.str2}' not found")
 
-	for i in range(maxlen):
-		letter1=str1[i:i+1]
-		letter2=str2[i:i+1]
-		if letter1 != letter2:
-			result1+=letter1
-			result2+=letter2
+	def compare(self):
+		if self.str1 == self.str2:
+			raise ValueError("Strings are identical.")
 
-	if result1 == "" and result2 == "":
-		raise ValueError("Strings are identical.")
+		result1 = ''
+		result2 = ''
 
-	print ("Letters different in string 1:",result1)
-	print ("Letters different in string 2:",result2)
+		maxlen=len(self.str2) if len(self.str1)<len(self.str2) else len(self.str1)
 
-compare()
+		for i in range(maxlen):
+			letter1=self.str1[i:i+1]
+			letter2=self.str2[i:i+1]
+			if letter1 != letter2:
+				result1+=letter1
+				result2+=letter2
+			else:
+				result1+="_"
+				result2+="_"
 
+		if result1 == "" and result2 == "":
+			raise ValueError("Strings are identical.")
+
+		print ("Letters different in string 1:",result1)
+		print ("Letters different in string 2:",result2)
+
+	def main(self):
+		print("Compare 2 strings and point out differences: ")
+		choice = input("Str/file: ")
+		if choice.lower() == "str":
+			self.strr()
+		elif choice.lower() == "file":
+			self.file()
+		else:
+			raise ValueError("invalid argument, should be: [str/file]")
+		self.compare()
+
+f = Compare()
+f.main()
